8f2811ee25f6c944203355de69ca4a4d084a7664
Xie Yi <yxie.lhi@gmail.com>
2025-06-04 00:08:21 -0400

    update

--- run.sh
+++ run.sh
@@ -22,13 +22,9 @@ python3 run_alphafold.py \
   --uniref90_database_path=${DATA_DIR}/uniref90/uniref90.fasta \
   --mgnify_database_path=${DATA_DIR}/mgnify/mgy_clusters_2022_05.fa \
   --template_mmcif_dir=${DATA_DIR}/pdb_mmcif/mmcif_files \
-  --obsolete_pdbs_path=${DATD_DIR}/pdb_mmcif/obsolete.dat \
-  --uniprot_database_path=${DATA_DIR}/uniprot/uniprot.fasta \
-  --pdb_seqres_database_path=${DATA_DIR}/pdb_seqres/pdb_seqres.txt \
+  --obsolete_pdbs_path=${DATA_DIR}/pdb_mmcif/obsolete.dat \
   --pdb70_database_path=${DATA_DIR}/pdb70/pdb70 \
   --small_bfd_database_path=${DATA_DIR}/small_bfd/bfd-first_non_consensus_sequences.fasta \
-  --uniref30_database_path=${DATA_DIR}/uniref30/UniRef30_2021_03 \
-  --bfd_database_path=${DATA_DIR}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
   --jackhmmer_binary_path=${ENV_PATH}/bin/jackhmmer \
   --hhblits_binary_path=${ENV_PATH}/bin/hhblits \
   --hhsearch_binary_path=${ENV_PATH}/bin/hhsearch \
