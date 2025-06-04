a46af6e0561197c6710df3764e8cd88d9e70a1e5
Xie Yi <yxie.lhi@gmail.com>
2025-06-04 05:08:57 -0400

    update

--- run_docker.sh
+++ run_docker.sh
@@ -11,5 +11,7 @@ INPUT_PATH=/home/xieyi/short.fasta
 python3 docker/run_docker.py \
   --fasta_paths=${INPUT_PATH} \
   --max_template_date=2020-05-14 \
+  --model_preset=monomer \
+  --db_preset=reduced_dbs \
   --data_dir=${DATA_DIR} \
   --output_dir=${OUTPUT_DIR}
