5cdadc291161a78d245a8350536cd0726b90ba81
williamxieigs <yi.xie@igsl-group.com>
2025-06-11 16:47:14 +0800

    update

--- frontend/manager.py
+++ frontend/manager.py
@@ -194,7 +194,7 @@ def create_interface(manager):
           if res is None: continue
           ranks.append(res[1])
           names.append(f'<a href="http://localhost:8081?path={join(output_dir, f)}" target="_blank">{stem}</a>')
-      return pd.DataFrame({'rank': ranks, 'name': names})
+      return pd.DataFrame({'rank': ranks, 'name(click to view)': names})
     # 3) events
     gpu_status_tab.select(
       update_status,
